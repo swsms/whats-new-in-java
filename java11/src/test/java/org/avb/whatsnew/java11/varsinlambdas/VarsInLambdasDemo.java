6c0c84390d377ea40090bd855435094fcb13405a
art <swsms@mail.ru>
2018-10-28 20:12:52 +0500

    Add example with var in lambda parameters

---
+++ java11/src/test/java/org/avb/whatsnew/java11/varsinlambdas/VarsInLambdasDemo.java
@@ -0,0 +1,29 @@
+package org.avb.whatsnew.java11.varsinlambdas;
+
+import org.avb.whatsnew.java11.strings.StringsMethodsDemo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import java.lang.annotation.*;
+import java.util.List;
+
+/**
+ * Java 11 introduces Local-Variable syntax for lambda parameters.
+ * It is used for one specific case like (@Annotation var param) -> ...
+ */
+public class VarsInLambdasDemo {
+    private static final Logger LOGGER = LoggerFactory.getLogger(VarsInLambdasDemo.class);
+
+    @Test
+    public void testVarInLambdas() {
+        List.of("Java", "Kotlin", "Scala", "Closure").stream()
+                .filter((@NotNull var lang) -> lang.startsWith("J"))
+                .forEach(LOGGER::info); // prints "Java"
+    }
+}
+
+@Target(ElementType.PARAMETER)
+@Inherited
+@Retention(RetentionPolicy.CLASS)
+@interface NotNull { }
