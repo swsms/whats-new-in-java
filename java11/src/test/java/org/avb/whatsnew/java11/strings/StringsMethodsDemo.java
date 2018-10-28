7e90831dd9a55b610981832a5902d3e6fa61bf6b
art <swsms@mail.ru>
2018-10-28 18:34:45 +0500

    Add demo of new string methods

--- java11/src/test/java/org/avb/whatsnew/java11/strings/StringsMethodsDemo.java
+++ java11/src/test/java/org/avb/whatsnew/java11/strings/StringsMethodsDemo.java
@@ -2,7 +2,6 @@ package org.avb.whatsnew.java11.strings;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -26,5 +25,24 @@ public class StringsMethodsDemo {
 
         var stringWithLetters = "  abc  ";
         assertFalse(stringWithLetters.isBlank());
+    }
+
+    @Test
+    public void testLines() {
+        var languages = "java\nkotlin\nscala\nclosure";
+        languages.lines()
+                .forEach(LOGGER::info);  // prints each word from a new line
+    }
+
+    @Test
+    public void testStripMethods() {
+        LOGGER.info("\n\t abc".stripLeading()); // "abc"
+        LOGGER.info("def\n\t ".stripTrailing()); // "def"
+        LOGGER.info(" \n\tqq\n\t ".strip()); // "qq"
+
+        var stringWithMagicSpace = "\u205Fabc";
+
+        LOGGER.info(stringWithMagicSpace.strip()); // "abc"
+        LOGGER.info(stringWithMagicSpace.trim());  // " abc"
     }
 }
